168840a7628d2d710ba3a897dc370f967e2d015a
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2024-09-29 11:06:22 +0530

    Update contact.js

--- Frontend/src/Components/contact.js
+++ Frontend/src/Components/contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex justify-center items-center min-h-screen bg-gray-100 font-roboto">
       <div className="bg-white p-8 rounded shadow-md text-center">
         <h1 className="text-4xl font-bold mb-4">COMING SOON</h1>
         <p className="text-3xl">😊</p>
