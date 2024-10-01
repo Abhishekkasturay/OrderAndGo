9cca88d87a6326268ef1ab81d06c55c1a9aab6a9
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2024-10-02 02:09:15 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -20,6 +20,11 @@ const userSchema = new mongoose.Schema({
 // Hash the password before saving the user model
 userSchema.pre("save", async function (next) {
   try {
+    // Only hash the password if it has been modified (or is new)
+    if (!this.isModified('password')) {
+      return next();
+    }
+
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
