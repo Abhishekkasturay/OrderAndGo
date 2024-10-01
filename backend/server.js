5a19460f1f2746f470932e018a9e9be1a2e85c17
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2024-10-02 04:02:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use("/api/auth", authRoutes);
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend/dist", "src/index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
