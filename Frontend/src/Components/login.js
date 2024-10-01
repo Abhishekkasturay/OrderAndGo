786449a2eb8ac7af513fc4696068840ea3df7665
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2024-10-02 02:10:45 +0530

    Update login.js

--- Frontend/src/Components/login.js
+++ Frontend/src/Components/login.js
@@ -17,7 +17,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("https://orderandgo-2.onrender.com/api//auth/login", {
+      const response = await fetch("https://orderandgo-2.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
