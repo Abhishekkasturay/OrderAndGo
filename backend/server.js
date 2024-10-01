e6938f6bdca1b4252b4c1d323aeafdeef7116333
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2024-10-02 04:59:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,10 +34,10 @@ mongoose
 app.use("/api/auth", authRoutes);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "Frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "Frontend/dist", "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
