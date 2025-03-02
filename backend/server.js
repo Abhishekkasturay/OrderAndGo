8aac593e0c13e6dffaaf0cd7fac7fa1e413c162b
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2025-03-02 22:45:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,34 +30,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/trains", trainRoutes);
 app.use("/api/gemini", geminiRoute);
 
-app.get('/api/swiggy', async (req, res) => {
-  try {
-    const { lat, lng, restaurantId } = req.query;
 
-    const swiggyUrl = `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=${restaurantId}`;
-
-    const response = await fetch(swiggyUrl, {
-      method: 'GET',
-    });
-
-    if (!response.ok) {
-      throw new Error(`Failed to fetch from Swiggy: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-
-    res.json(data);
-  } catch (error) {
-    console.error('Error fetching Swiggy data:', error);
-    res.status(500).send('Error fetching data from Swiggy');
-  }
-});
-
-app.use(express.static(path.join(__dirname, "frontend/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
-});
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
